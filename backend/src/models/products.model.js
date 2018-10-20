abb19625e969f392c897a0728643cedf8e03dcb5
lagmanzaza <lagmanmc@gmail.com>
2018-10-20 20:00:12 +0700

    [Service,Mongoose] product model

---
+++ backend/src/models/products.model.js
@@ -0,0 +1,26 @@
+// products-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const products = new Schema({
+    shopId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+  }, {
+    timestamps: true,
+  });
+
+  return mongooseClient.model('products', products);
+};
